49fa6958411a5982f40b89437810a35870b26944
Pradip Bhandari <Pradip Bhandari>
2016-02-28 10:18:35 +0545

    Initial commit

---
+++ src/com/masterpradipg/helloWorld/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.masterpradipg.helloWorld;
+
+/**
+ * Created by Pradip on 2/12/2016.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("HEllo World");
+    }
+}
